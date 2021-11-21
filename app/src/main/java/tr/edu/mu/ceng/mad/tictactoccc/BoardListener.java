cf1baa17022f33bbe6ae0095144d70be16d7a3ff
U180709012 <enesdertli@posta.mu.edu.tr>
2021-11-21 15:55:10 +0300

    Initial commit

---
+++ app/src/main/java/tr/edu/mu/ceng/mad/tictactoccc/BoardListener.java
@@ -0,0 +1,10 @@
+package tr.edu.mu.ceng.mad.tictactoccc;
+
+public interface BoardListener {
+    byte NO_ONE = 0;
+    byte PLAYER_1 = 1;
+    byte PLAYER_2 = 2;
+    void playedAt(byte player, byte row, byte col);
+    void gameEnded(byte winner);
+}
+
